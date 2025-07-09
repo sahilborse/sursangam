53d2fdb825254607c771c137d0b9f5dcfe344dfb
sahilborse <sahilborse.909098@gmail.com>
2025-07-09 16:00:12 +0530

    Added dynamic Url

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: `${process.env.BACKEND_API_URL || 'http://localhost:5000'}`,
     // withCredentials: true,
 });
 
